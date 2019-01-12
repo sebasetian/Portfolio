06f3313b482b692b2fee3124de24bcbf31a5adb7
shih-yao <sebasetian123@gmail.com>
2019-01-12 11:23:25 -0800

    starter code

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8081;
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '../react-client/dist')))
+app.set('port', port)
+app.get('/', (req,res) => {
+    res.sendFile('index.html')
+})
+
+app.listen(port, () => console.log(`this blog is listening on port ${port}`))
