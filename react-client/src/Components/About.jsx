76f0f36f59e97a74936f51312c5ebe499b85b8d5
shih-yao <sebasetian123@gmail.com>
2019-01-14 14:24:58 -0800

    About me

--- react-client/src/Components/About.jsx
+++ react-client/src/Components/About.jsx
@@ -10,25 +10,23 @@ export default class AboutPage extends Component {
                 <Grid divided='vertically'>
                     <Grid.Row>
                         <Grid.Column verticalAlign='middle' width={3}>
-                            <Header as='h2' textAlign='center' image={avatar} content='About me' />
+                            <Header as='h2' textAlign='center' image={avatar} content='About Me' />
                         </Grid.Column>
                         <Grid.Column width={13}>
-                            I enjoy solving some of the hardest, most challenging problems in technology.
-                            The kind of problems I want to solve require diverse perspectives and the team-spirit to see those differences as opportunities rather than roadblocks.
+                        <p class='text'>
+                            I enjoy solving some of the hardest, most challenging problems in technology. <br />
+                            The kind of problems I want to solve require diverse perspectives and the team-spirit to see those differences as opportunities rather than roadblocks. <br />
                             I am looking forward to gaining hand-on experience and making tech impacts in software development,
                             also making some interesting projects applying NLP techniques I have learnt during my graduate studies. 
+                        </p>
                         </Grid.Column>
                     </Grid.Row>
-
-                    <Grid.Row columns={3}>
-                        <Grid.Column>
-                            <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+                    <Grid.Row>
+                        <Grid.Column verticalAlign='middle' width={3}>
+                            <Header as='h2' textAlign='center' icon='code' content='Tech Stack' />
                         </Grid.Column>
-                        <Grid.Column>
-                            <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+                        <Grid.Column width={13}>
+                            
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
